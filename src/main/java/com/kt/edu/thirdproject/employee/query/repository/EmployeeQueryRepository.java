7b380a87ec0a56c514a48fb21131b6741790ad20
heewon00 <55778040+heewon00@users.noreply.github.com>
2024-03-04 17:37:02 +0900

    240304 CQRS_수정

--- src/main/java/com/kt/edu/thirdproject/employee/query/repository/EmployeeQueryRepository.java
+++ src/main/java/com/kt/edu/thirdproject/employee/query/repository/EmployeeQueryRepository.java
@@ -1,18 +1,11 @@
 package com.kt.edu.thirdproject.employee.query.repository;
 
 import com.kt.edu.thirdproject.employee.query.domain.EmployeeEntity;
-import com.kt.edu.thirdproject.employee.query.repository.sql.QueryEmployeeSqls;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeQueryRepository extends CrudRepository<EmployeeEntity, Long> {
 
-    @Query(QueryEmployeeSqls.RETV_NEXT_VAL)
-    Long retvNextVal();
-
-    @Query(QueryEmployeeSqls.RETV_NEXT_VAL_H2)
-    Long retvNextVal_H2();
     
 }
